cc763cdd48decdf66297ccb1a6a30e0263d0cdc8
Den <denisanatoliyovich@gmail.com>
2020-07-07 11:55:23 +0300

    Updates

---
+++ precache-manifest.74d90ca0107edbde1d6a8752b3829192.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4273e0632975824b0966395f47ab7564",
+    "url": "/sea-battle/index.html"
+  },
+  {
+    "revision": "b42bc89165646ebbb1b2",
+    "url": "/sea-battle/static/css/main.5341233e.chunk.css"
+  },
+  {
+    "revision": "851a4d00ff69be10db3f",
+    "url": "/sea-battle/static/js/2.1a7cdb87.chunk.js"
+  },
+  {
+    "revision": "b42bc89165646ebbb1b2",
+    "url": "/sea-battle/static/js/main.e2cc0fd3.chunk.js"
+  },
+  {
+    "revision": "bacc1e412213acbdb6ca",
+    "url": "/sea-battle/static/js/runtime~main.db7f1d8f.js"
+  }
+]);
